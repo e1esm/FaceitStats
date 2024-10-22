d1eb476e4e79827bd09b281f9a647063dac22dce
elesm <elesmdev1102@gmail.com>
2024-10-22 21:42:00 +0300

    init commit

---
+++ src/main/java/com/esm/faceitstats/FaceitStatsApplication.java
@@ -0,0 +1,13 @@
+package com.esm.faceitstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceitStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceitStatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esm/faceitstats/FaceitStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esm.faceitstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceitStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
